d1ac03ae9f105f595f7caa0196fb2d47f6c3c193
Pramath Hegde <01fe22bca137@kletech.ac.in>
2024-02-20 10:35:11 +0530

    Code for extended calculator

---
+++ Extended_calculator.java
@@ -0,0 +1,13 @@
+  /**
+   * The Extendedcalculator class is a Java class that likely contains additional functionality beyond
+   * a basic calculator.
+   */
+  class Extendedcalculator {
+    float square(float oparend_1){
+        return(oparend_1*oparend_1);
+    }
+    float  cube(float oparend_2){
+        return(oparend_2*oparend_2*oparend_2);
+    }
+}
+
